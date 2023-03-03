13f9e15bc7c42ed7394c30d376291379f237e550
baokunchao <baokunchao1@huawei.com>
2023-03-03 14:40:51 +0800

    IssueNo:#I6JCFI 支持模块升级调试工具 Description: add debug tool Sig:sys_installer Feature or Bugfix:Bugfix Binary Source:No
    Change-Id: I0ff4e636aa142c0ffdbc3af57801df5e002466d2
    Signed-off-by: baokunchao <baokunchao1@huawei.com>

---
+++ tools/module_update_tool/main.cpp
@@ -0,0 +1,132 @@
+# Copyright (c) 2023 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#include <cstring>
+
+#include "module_update_kits.h"
+#include "module_error_code.h"
+
+static const int32_t MIN_PARAM_NUM = 2;
+static const int32_t MAX_PARAM_NUM = 3;
+
+static const std::string HELP_MSG = 
+    "usage: module_update_tool\n"
+    "example: ./module_update_tool install /data/tmp/xxx.hmp \n"
+    "command list:\n"
+    "  install        : upgrade some SA via hmp package"
+    "  uninstall      : degrade some SA via hmp name"
+    "  show hmpname   : show upgrade sa info, if hmap name is null, show all";
+
+static const std::string INSTALL_PARAM = "install";
+static const std::string UNINSTALL_PARAM = "uninstall";
+static const std::string SHOW_INFO = "show";
+
+enum ModuleUpdateToolRet {
+    RET_FAILED = -1,
+    RET_SUCCESS = 0,
+};
+
+bool CheckParam(int argc)
+{
+    if (argc < MIN_PARAM_NUM || argc > MAX_PARAM_NUM) {
+        printf("Invalid module update command");
+        return false;
+    }
+    return true;
+}
+
+std::string GetFailReasonByErrCode(int32_t err)
+{
+    switch (err) {
+        case 0:
+            return "success";
+        case OHOS::SysInstaller::ERR_SERVICE_NOT_FOUND:
+            return "ERR_SERVICE_NOT_FOUND";
+        case OHOS::SysInstaller::ERR_INVALID_PATH:
+            return "ERR_INVALID_PATH";
+        case OHOS::SysInstaller::ERR_LOWER_VERSION:
+            return "ERR_LOWER_VERSION";
+        case OHOS::SysInstaller::ERR_VERIFY_SIGN_FAIL:
+            return "ERR_VERIFY_SIGN_FAIL";
+        case OHOS::SysInstaller::ERR_INSTALL_FAIL:
+            return "ERR_INSTALL_FAIL";
+        case OHOS::SysInstaller::ERR_UNINSTALL_FAIL:
+            return "ERR_UNINSTALL_FAIL";
+        case OHOS::SysInstaller::ERR_REPORT_STATUS_FAIL:
+            return "ERR_REPORT_STATUS_FAIL";
+        default:
+            return "Unknown Error";
+    }
+}
+
+void PrintErrMsg(std::string errMsg)
+{
+    printf("%s", errMsg.c_str());
+    printf("\n");
+}
+
+void PrintUpgradeInfo(std::list<OHOS::SysInstaller::ModulePackageInfo> &modulePackageInfos)
+{
+    std::list<OHOS::SysInstaller::ModulePackageInfo>::iterator it;
+    for (it = modulePackageInfos.begin(); it != modulePackageInfos.end(); it++) {
+        OHOS::SysInstaller::ModulePackageInfo moduleInfo = *it;
+        printf("%s\n", moduleInfo.hmpName.c_str());
+        std::list<OHOS::SysInstaller::SaInfo>::iterator saIt;
+        for (saIt = moduleInfo.saInfoList.begin(); saIt != moduleInfo.saInfoList.end(); saIt++) {
+            std::string verStr = (*saIt).version;
+            printf(" {saName:%s saId:%d version:%s}\n", (*saIt).saName.c_str(), (*saIt).saId, verStr.c_str());
+        }
+        printf(" \n");
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (!CheckParam(argc)) {
+        return RET_FAILED;
+    }
+
+    int32_t ret = 0;
+    OHOS::SysInstaller::ModuleUpdateKits& moduleUpdateKits = OHOS::SysInstaller::ModuleUpdateKits::GetInstance();
+    ret = moduleUpdateKits.InitModuleUpdate();
+    if (ret != 0) {
+        PrintErrMsg(GetFailReasonByErrCode(ret));
+        return ret;
+    }
+
+    if (INSTALL_PARAM.compare(argv[1] == 0) && argc == MAX_PARAM_NUM) {
+        printf("try to update a mudule\n");
+        ret = moduleUpdateKits.InstallModulePackage(argv[2]);
+        PrintErrMsg(GetFailReasonByErrCode(ret));
+        return ret;
+    } 
+    if (UNINSTALL_PARAM.compare(argv[1] == 0) && argc == MAX_PARAM_NUM) {
+        printf("try to uninstall an upgrade package\n");
+        ret = moduleUpdateKits.UninstallModulePackage(argv[2]);
+        PrintErrMsg(GetFailReasonByErrCode(ret));
+        return ret;
+    } 
+    if (SHOW_INFO.compare(argv[1] == 0) {
+        printf("try to show module update info\n");
+        std::list<OHOS::SysInstaller::ModulePackageInfo> modulePackageInfos;
+        std::string hmpStr;
+        const std::string &hmpName = (argc == MIN_PARAM_NUM ? NULL : hmpStr = argv[2]);
+        ret = moduleUpdateKits.GetModulePackageInfo(hmpName, modulePackageInfos);
+        PrintUpgradeInfo(modulePackageInfos);
+        PrintErrMsg(GetFailReasonByErrCode(ret));
+        return ret;
+    }
+    
+    printf("invalid command. \n");
+    return ret;
+}
