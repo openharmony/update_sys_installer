2c9d85111cc634e28502c3ddd266bf78c786f417
蓝色大魔王 <hanqiaosheng@huawei.com>
2023-09-25 14:23:07 +0000

    update frameworks/ipc_server/src/module_update_stub.cpp.
    Signed-off-by: 蓝色大魔王 <hanqiaosheng@huawei.com>

--- frameworks/ipc_server/src/module_update_stub.cpp
+++ frameworks/ipc_server/src/module_update_stub.cpp
@@ -15,11 +15,15 @@
 
 #include "module_update_stub.h"
 
+#include "accesstoken_kit.h"
+#include "ipc_skeleton.h"
 #include "isys_installer_callback.h"
 #include "sys_installer_sa_ipc_interface_code.h"
 
 #include "log/log.h"
 #include "string_ex.h"
+
+constexpr int USER_UPDATE_AUTHORITY = 6666;
 
 namespace OHOS {
 namespace SysInstaller {
@@ -177,6 +181,29 @@ int32_t ModuleUpdateStub::GetHmpUpdateResultStub(ModuleUpdateStub *service,
     return 0;
 }
 
+bool ModuleUpdateStub::IsPermissionGranted(void)
+{
+    Security::AccessToken::AccessTokenID callerToken = IPCSkeleton::GetCallingTokenID();
+    std::string permission = "ohos.permission.UPDATE_SYSTEM";
+
+    int verifyResult = Security::AccessToken::AccessTokenKit::VerifyAccessToken(callerToken, permission);
+    bool isPermissionGranted = (verifyResult == Security::AccessToken::PERMISSION_GRANTED);
+    if (!isPermissionGranted) {
+        LOG(ERROR) << "not granted " << permission.c_str();
+    }
+    return isPermissionGranted;
+}
+
+bool ModuleUpdateStub::CheckCallingPerm(void)
+{
+    int32_t callingUid = OHOS::IPCSkeleton::GetCallingUid();
+    LOG(INFO) << "CheckCallingPerm callingUid:" << callingUid;
+    if (callingUid == 0) {
+        return true;
+    }
+    return callingUid == USER_UPDATE_AUTHORITY && IsPermissionGranted();
+}
+
 int32_t ModuleUpdateStub::OnRemoteRequest(uint32_t code,
     MessageParcel &data, MessageParcel &reply, MessageOption &option)
 {
@@ -188,6 +215,10 @@ int32_t ModuleUpdateStub::OnRemoteRequest(uint32_t code,
     LOG(INFO) << "OnRemoteRequest func code " << code;
     auto inter = requestFuncMap_.find(code);
     if (inter != requestFuncMap_.end()) {
+        if (!CheckCallingPerm()) {
+            LOG(ERROR) << "ModuleUpdateStub CheckCallingPerm fail";
+            return -1;
+        }
         return inter->second(this, data, reply, option);
     }
 
