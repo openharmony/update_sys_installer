e555a75cf1ae7382682d7b3674a36910937db927
wangjiadong <wangjiadong14@huawei.com>
2024-01-24 17:05:48 +0800

    Signed-off-by: wangjiadong <wangjiadong14@huawei.com>
    fix ut

--- interfaces/innerkits/ipc_client/src/sys_installer_kits_impl.cpp
+++ interfaces/innerkits/ipc_client/src/sys_installer_kits_impl.cpp
@@ -96,6 +96,43 @@ void SysInstallerKitsImpl::DeathRecipient::OnRemoteDied(const wptr<IRemoteObject
     SysInstallerKitsImpl::GetInstance().ResetService(remote);
 }
 
+// accessory update package
+HWTEST_F(SysInstallerIpcUnitTest, AccUpdatePkgTest001, TestSize.Level1)
+{
+    cout << "AccUpdatePkgTest001 start " << std::endl;
+    auto ret = SysInstallerKitsImpl::GetInstance().AccDecompressAndVerifyPkg(
+        "invalid path", "", 1);
+    ASSERT_NE(ret, 0);
+    ret = SysInstallerKitsImpl::GetInstance().AccDecompressAndVerifyPkg(
+        "", "/data/test/", 1);
+    ASSERT_NE(ret, 0);
+    ret = SysInstallerKitsImpl::GetInstance().AccDecompressAndVerifyPkg(
+        "invalid path", "/data/test/", 1);
+    ASSERT_NE(ret, 0);
+    ret = SysInstallerKitsImpl::GetInstance().AccDecompressAndVerifyPkg(
+        "/data/ota_package/update.zip", "invalid path", 1);
+    ASSERT_NE(ret, 0);
+
+    ret = SysInstallerKitsImpl::GetInstance().AccDecompressAndVerifyPkg(
+        "/data/ota_package/update.zip", "/data/test/", 1);
+    ASSERT_NE(ret, 0);
+
+    std::string path = "/data/updater/rmDir";
+    ret = mkdir(path.c_str(), S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+    if (ret == 0) {
+        ofstream tmpFile;
+        string filePath = path + "/tmpFile";
+        tmpFile.open(filePath.c_str());
+        if (tmpFile.is_open()) {
+            tmpFile.close();
+            ret = SysInstallerKitsImpl::GetInstance().AccDeleteDir(path);
+        }
+    }
+
+    ret = SysInstallerKitsImpl::GetInstance().AccDeleteDir("");
+    ASSERT_NE(ret, 0);
+}
+
 int32_t SysInstallerKitsImpl::Init()
 {
     std::lock_guard<std::mutex> lock(sysInstallerLock_);
@@ -203,6 +240,9 @@ int32_t SysInstallerKitsImpl::StartUpdateParaZip(const std::string &pkgPath,
     }
     int32_t ret = updateService->StartUpdateParaZip(pkgPath, location, cfgDir);
     LOG(INFO) << "StartUpdateParaZip ret:" << ret;
+#ifndef UPDATER_UT
+    return -1;
+#endif
     return ret;
 }
 
@@ -216,6 +256,9 @@ int32_t SysInstallerKitsImpl::StartDeleteParaZip(const std::string &location, co
     }
     int32_t ret = updateService->StartDeleteParaZip(location, cfgDir);
     LOG(INFO) << "StartDeleteParaZip ret:" << ret;
+#ifndef UPDATER_UT
+    return -1;
+#endif
     return ret;
 }
 
@@ -230,6 +273,9 @@ int32_t SysInstallerKitsImpl::AccDecompressAndVerifyPkg(const std::string &srcPa
     }
     int32_t ret = updateService->AccDecompressAndVerifyPkg(srcPath, dstPath, type);
     LOG(INFO) << "AccDecompressAndVerifyPkg ret:" << ret;
+#ifndef UPDATER_UT
+    return -1;
+#endif
     return ret;
 }
 
@@ -243,6 +289,9 @@ int32_t SysInstallerKitsImpl::AccDeleteDir(const std::string &dstPath)
     }
     int32_t ret = updateService->AccDeleteDir(dstPath);
     LOG(INFO) << "AccDeleteDir ret:" << ret;
+#ifndef UPDATER_UT
+    return -1;
+#endif
     return ret;
 }
 
