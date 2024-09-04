b4f41c8856a106a56787ae0749358f65db7275f2
xieluyao <xieluyao2@h-partners.com>
2024-09-04 02:28:36 +0000

    update interfaces/innerkits/ipc_client/src/main.cpp.
    Signed-off-by: xieluyao <xieluyao2@h-partners.com>

--- interfaces/innerkits/ipc_client/src/main.cpp
+++ interfaces/innerkits/ipc_client/src/main.cpp
@@ -1,17 +1,17 @@
 /*
-* Copyright (c) 2023 Huawei Device Co., Ltd.
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* you may obtain a copy of the License at
-*
-* http://www.apache.org/license/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/license/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <unistd.h>
 
@@ -22,11 +22,11 @@
 #include "module_update_kits_impl.h"
 
 using namespace OHOS;
-using namespace Update;
+using namespace Updater;
 
-int main ()
+int main()
 {
-    InitUpdaterLogger("ModuleUpdaterClient","","","");
+    InitUpdaterLogger("ModuleUpdaterClient", "", "", "");
     sptr<ISystemAbilityManager> samgr = nullptr;
     int32_t times = 30;
     const int32_t wiat_time_ms = 100 * 1000;
@@ -41,11 +41,13 @@ int main ()
             break;
         }
     }
+
     if (samgr == nullptr) {
-        LOG(ERROR) <<"ModuleUpdateInit wait samgr fail",
+        LOG(ERROR) <<"ModuleUpdateInit wait samgr fail";
         return -1;
     }
+
     int32_t ret = OHOS::SysInstaller::ModuleUpdateKits::GetInstance().InitModuleUpdate();
-    LOG(INFO) << "ModuleUpdateInitret: " << ret;
+    LOG(INFO) << "ModuleUpdateInit ret: " << ret;
     return ret;
 }
