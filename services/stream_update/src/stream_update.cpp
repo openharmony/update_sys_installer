fcb5493082a114ab01f2f49ab758085eea647f9c
甘罗宇 <ganluoyu@talkweb.com.cn>
2025-03-31 09:49:01 +0800

    修改代码格式
    Signed-off-by: 甘罗宇 <ganluoyu@talkweb.com.cn>
    Change-Id: I59fa7e83e60ef5148c410dad2425b74584049e97

--- services/stream_update/src/stream_update.cpp
+++ services/stream_update/src/stream_update.cpp
@@ -49,6 +49,7 @@ uint16_t ReadLE16(const uint8_t *buff, size_t length)
     }
     if (length < sizeof(uint16_t)) {
         LOG(ERROR) << "Buffer length is insufficient for ReadLE16";
+        return 0;
     }
     uint16_t value16 = buff[0];
     value16 += static_cast<uint16_t>(buff[1] << ONE_BYTE);
@@ -63,6 +64,7 @@ uint32_t ReadLE32(const uint8_t *buff, size_t length)
     }
     if (length < sizeof(uint32_t)) {
         LOG(ERROR) << "Buffer length is insufficient for ReadLE32";
+        return 0;
     }
     uint16_t low = ReadLE16(buff, length);
     uint16_t high = ReadLE16(buff + sizeof(uint16_t), length - sizeof(uint16_t));
@@ -124,78 +126,78 @@ void StreamInstallProcesser::ThreadExecuteFunc()
         uint8_t temporaryBuffer[BUFFER_SIZE]{0};
         uint32_t len = 0;
         if (!ringBuffer_.Pop(temporaryBuffer, sizeof(temporaryBuffer), len)) {
-            if (!partial_data_.empty()) {
+            if (!partialData_.empty()) {
                 ProcessPartialData();
             }
             break;
         }
 
-        partial_data_.insert(partial_data_.end(), temporaryBuffer, temporaryBuffer + len);
+        partialData_.insert(partialData_.end(), temporaryBuffer, temporaryBuffer + len);
         ProcessPartialData();
     }
 }
 
 bool StreamInstallProcesser::ProcessHeader()
 {
-    if (header_processed_) {
+    if (headerProcessed_) {
         return true;
     }
 
-    if (partial_data_.size() < TOTAL_TL_BYTES) {
+    if (partialData_.size() < TOTAL_TL_BYTES) {
         return false;
     }
     
-    const uint16_t type = ReadLE16(partial_data_.data(), partial_data_.size());
+    const uint16_t type = ReadLE16(partialData_.data(), partialData_.size());
     if (type != ZIP_HEADER_TLV_TYPE) {
-        header_processed_ = true;
+        headerProcessed_ = true;
         LOG(ERROR) << "No match type: " << type;
         return true;
     }
-    const uint32_t length = ReadLE32(partial_data_.data() + 2, partial_data_.size() - 2);
+    const uint32_t length = ReadLE32(partialData_.data() + 2, partialData_.size() - 2);
     LOG(INFO) << "Length = " << length;
-    skip_remaining_ = length;
-    partial_data_.erase(partial_data_.begin(), partial_data_.begin() + TOTAL_TL_BYTES);
-    header_processed_ = true;
+    skipRemaining_ = length;
+    partialData_.erase(partialData_.begin(), partialData_.begin() + TOTAL_TL_BYTES);
+    headerProcessed_ = true;
 
     return true;
 }
 
 bool StreamInstallProcesser::SkipTargetData()
 {
-    if (skip_remaining_ <= 0) {
-        LOG(ERROR) << "no valid skip_remaining_ = " << skip_remaining_;
+    if (skipRemaining_ <= 0) {
+        LOG(ERROR) << "no valid skipRemaining_ = " << skipRemaining_;
         return false; // 不需要跳过数据，直接返回
     }
-    const size_t skip = std::min<size_t>(skip_remaining_, partial_data_.size());
-    partial_data_.erase(partial_data_.begin(), partial_data_.begin() + skip);
-    skip_remaining_ -= skip;
-    LOG(INFO) << "skip_remaining_ = " << skip_remaining_;
+    const size_t skip = std::min<size_t>(skipRemaining_, partialData_.size());
+    partialData_.erase(partialData_.begin(), partialData_.begin() + skip);
+    skipRemaining_ -= skip;
+    LOG(INFO) << "skipRemaining_ = " << skipRemaining_;
     return true; // 跳过数据后继续循环
 }
 
 bool StreamInstallProcesser::ProcessValidData()
 {
-    const size_t process_size = std::min<size_t>(partial_data_.size(), BUFFER_SIZE);
-    if (process_size == 0) {
+    const size_t processSize = std::min<size_t>(partialData_.size(), BUFFER_SIZE);
+    if (processSize == 0) {
         return true; // 无数据可处理，退出循环
     }
     uint8_t buffer[BUFFER_SIZE]{0};
-    errno_t ret = memcpy_s(buffer, BUFFER_SIZE, partial_data_.data(), process_size);
+    errno_t ret = memcpy_s(buffer, BUFFER_SIZE, partialData_.data(), processSize);
     if (ret != 0) {
         LOG(ERROR) << "ProcessStreamData memcpy_s failed: " << ret;
         return false;
     }
-    partial_data_.erase(partial_data_.begin(), partial_data_.begin() + process_size);
+    partialData_.erase(partialData_.begin(), partialData_.begin() + processSize);
     uint32_t dealLen = 0;
-    const UpdateResultCode ret_update = binChunkUpdate_->StartBinChunkUpdate(buffer, process_size, dealLen);
-    if (ret_update == STREAM_UPDATE_SUCCESS) {
+    const UpdateResultCode retUpdate = binChunkUpdate_->StartBinChunkUpdate(buffer, processSize, dealLen);
+    if (retUpdate == STREAM_UPDATE_SUCCESS) {
         LOG(INFO) << "StreamInstallProcesser ThreadExecuteFunc STREAM_UPDATE_SUCCESS";
         UpdateResult(UPDATE_STATE_ONGOING, dealLen, "");
-    } else if (ret_update == STREAM_UPDATE_FAILURE) {
+    } else if (retUpdate == STREAM_UPDATE_FAILURE) {
         LOG(ERROR) << "StreamInstallProcesser ThreadExecuteFunc STREAM_UPDATE_FAILURE";
         UpdateResult(UPDATE_STATE_FAILED, dealLen, "");
         isExitThread_ = true;
-    } else if (ret_update == STREAM_UPDATE_COMPLETE) {
+    } else if (retUpdate == STREAM_UPDATE_COMPLETE) {
         LOG(INFO) << "StreamInstallProcesser ThreadExecuteFunc STREAM_UPDATE_COMPLETE";
         UpdateResult(UPDATE_STATE_SUCCESSFUL, dealLen, "");
         isExitThread_ = true;
@@ -205,7 +207,7 @@ bool StreamInstallProcesser::ProcessValidData()
 
 void StreamInstallProcesser::ProcessPartialData()
 {
-    while (!partial_data_.empty() && !isExitThread_) {
+    while (!partialData_.empty() && !isExitThread_) {
         // 阶段1：处理头部数据
         if (!ProcessHeader()) {
             break;
