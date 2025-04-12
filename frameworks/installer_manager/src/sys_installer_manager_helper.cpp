c3bfb3979710909144ebd8c689ba682a17b40885
陈凯 <chenkai172@huawei.com>
2025-04-12 06:14:37 +0000

    update frameworks/installer_manager/src/sys_installer_manager_helper.cpp.
    Signed-off-by: 陈凯 <chenkai172@huawei.com>

--- frameworks/installer_manager/src/sys_installer_manager_helper.cpp
+++ frameworks/installer_manager/src/sys_installer_manager_helper.cpp
@@ -131,5 +131,10 @@ int32_t SysInstallerManagerHelper::ClearVabMetadataAndCow()
 {
     return -1;
 }
+
+int32_t SysInstallerManagerHelper::MergeRollbackReasonFile()
+{
+    return -1;
+}
 } // namespace SysInstaller
 } // namespace OHOS
