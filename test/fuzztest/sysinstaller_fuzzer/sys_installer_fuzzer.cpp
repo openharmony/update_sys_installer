977dacb233158304db227d47808a17776a72d167
LeonShu <shuzhan10@huawei.com>
2025-04-18 03:50:49 +0000

    update test/fuzztest/sysinstaller_fuzzer/sys_installer_fuzzer.cpp.
    Signed-off-by: LeonShu <shuzhan10@huawei.com>

--- test/fuzztest/sysinstaller_fuzzer/sys_installer_fuzzer.cpp
+++ test/fuzztest/sysinstaller_fuzzer/sys_installer_fuzzer.cpp
@@ -43,7 +43,8 @@ void FuzzSysInstaller(const uint8_t* data, size_t size)
     SysInstallerKitsImpl::GetInstance().AccDeleteDir(std::string(reinterpret_cast<const char*>(data), size));
     SysInstallerKitsImpl::GetInstance().ClearVabMetadataAndCow();
     SysInstallerKitsImpl::GetInstance().MergeRollbackReasonFile();
-    SysInstallerKitsImpl::GetInstance().GetMetadataUpdateStatus(1);
+    int32_t metadataStatus = 0;
+    SysInstallerKitsImpl::GetInstance().GetMetadataUpdateStatus(metadataStatus);
     SysInstallerKitsImpl::GetInstance().StartVabMerge();
     SysInstallerKitsImpl::GetInstance().EnableVabCheckpoint();
     SysInstallerKitsImpl::GetInstance().AbortVabActiveSnapshot();
