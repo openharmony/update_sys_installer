c088b702e639f2f7144faa776fed2a9ca229bf1d
蓝色大魔王 <hanqiaosheng@huawei.com>
2025-05-22 22:00:33 +0800

    Signed-off-by:high_level <352754628@qq.com>

--- interfaces/innerkits/ipc_client/src/sys_installer_kits_impl.cpp
+++ interfaces/innerkits/ipc_client/src/sys_installer_kits_impl.cpp
@@ -453,6 +453,7 @@ int32_t SysInstallerKitsImpl::GetMetadataUpdateStatus(int32_t &metadataStatus)
 int32_t SysInstallerKitsImpl::VabUpdateActive()
 {
     LOG(INFO) << "VabUpdateActive";
+    sptr
     auto updateService = GetService();
     if (updateService == nullptr) {
         LOG(ERROR) << "Get updateService failed";
