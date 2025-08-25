a4c7c35f88c28c8562ad9a8ac1a641e9788a6fab
王志康 <wangzhikang1@huawei.com>
2025-08-25 03:54:30 +0000

    处理告警
    Signed-off-by: 王志康 <wangzhikang1@huawei.com>

--- services/module_update/util/src/module_hvb_utils.cpp
+++ services/module_update/util/src/module_hvb_utils.cpp
@@ -81,7 +81,7 @@ bool GetCertDataFromImage(int fd, uint64_t imageSize, uint64_t offset, uint8_t *
 
 bool WriteModuleUpdateBlock(const struct hvb_buf &pubkey, const std::string &partition)
 {
-    if (pubkey.size + HVB_FOOTER_SIZE > DEFAULT_MODULE_HVB_INFO_SIZE) {
+    if (pubkey.size > DEFAULT_MODULE_HVB_INFO_SIZE - HVB_FOOTER_SIZE) {
         LOG(ERROR) << "the size of pubkey and footer is override";
         return false;
     }
