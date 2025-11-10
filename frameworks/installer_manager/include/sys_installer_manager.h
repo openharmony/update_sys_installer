c7f22390b62c1d85db95da5f60f1929e8e849270
chenkai11 <chenkai172@h-partners.com>
2025-11-10 20:02:31 +0800

    update: 更新文件 sys_installer_manager.h
    Signed-off-by: chenkai11 <chenkai172@h-partners.com>

--- frameworks/installer_manager/include/sys_installer_manager.h
+++ frameworks/installer_manager/include/sys_installer_manager.h
@@ -40,6 +40,7 @@ public:
     int32_t AccDeleteDir(const std::string &taskId, const std::string &dstPath);
     int32_t CancelUpdateVabPackageZip(const std::string &taskId);
     int32_t StartUpdateVabPackageZip(const std::string &taskId, const std::vector<std::string> &pkgPath);
+    int32_t StartUpdateSingularPackageZip(const std::string &taskId, const std::vector<std::string> &pkgPath);
     int32_t CreateVabSnapshotCowImg(const std::unordered_map<std::string, uint64_t> &partitionInfo);
     int32_t CreateVabSnapshotCowImg(const std::string &name, uint64_t size, uint64_t splitSize,
         uint64_t &createdSize, bool &isCreated);
